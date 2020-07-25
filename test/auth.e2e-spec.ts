5fa3d523357a53c9de61510e82e44a11c9a2b05b
Bretton Auerbach <bretton.auerbach@gmail.com>
2020-07-24 18:04:28 -0700

    split tests into app & auth

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -3,9 +3,7 @@ import * as request from 'supertest';
 import { RegisterDTO, LoginDTO } from 'src/auth/auth.dto';
 import { HttpStatus } from '@nestjs/common';
 import * as mongoose from 'mongoose';
-import { async } from 'rxjs';
-
-const app = 'http://localhost:3000';
+import { app } from './constants';
 
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGO_URI)
@@ -16,22 +14,13 @@ afterAll(async done => {
   await mongoose.disconnect(done);
 });
 
-describe('ROOT', () => {
-  it('should ping', () => {
-    return request(app)
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
-
 describe('AUTH', () => {
-  it('should register', () => {
-    const user: RegisterDTO = {
-      username: 'username',
-      password: 'password'
-    };
+  const user: RegisterDTO | LoginDTO = {
+    username: 'username',
+    password: 'password'
+  };
 
+  it('should register', () => {
     return request(app)
       .post('/auth/register')
       .set('Accept', 'application/json')
@@ -45,37 +34,26 @@ describe('AUTH', () => {
   });
 
   it('should reject duplicate registration', () => {
-    const user: RegisterDTO = {
-      username: 'username',
-      password: 'password'
-    };
-
     return request(app)
       .post('/auth/register')
       .set('Accept', 'application/json')
       .send(user)
       .expect(({ body }) => {
         expect(body.message).toEqual('User already exists');
-        expect(body.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+        expect(body.code).toEqual(HttpStatus.BAD_REQUEST);
       })
       .expect(HttpStatus.BAD_REQUEST)
   });
 
   it('should login', () => {
-    const user: LoginDTO = {
-      username: 'username',
-      password: 'password'
-    };
-
     return request(app)
       .post('/auth/login')
       .set('Accept', 'application/json')
       .send(user)
       .expect(({ body }) => {
         expect(body).toBeDefined();
-        console.log(body);
-        // expect(body.user.username).toEqual('username');
-        // expect(body.user.password).toBeUndefined();
+        expect(body.user.username).toEqual('username');
+        expect(body.user.password).toBeUndefined();
       })
       .expect(HttpStatus.CREATED)
   });
